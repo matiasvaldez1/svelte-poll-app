d5bbb82a6567c6ab0e3a2f5973b46523e29c7715
Matias Valdez <matiasvaldez8184@gmail.com>
2022-07-17 12:35:19 -0300

    typo

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>LLora Mateo!</h1>
+	<h1>Hello world!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
